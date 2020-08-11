77626cb0d8a646580865f9b2f4cb44214eb93ba5
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2020-08-11 11:10:50 -0300

    created and implemented authentication middleware

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,13 +1,34 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import authConfig from './config/auth';
 
 import ClassesController from './controllers/ClassesController';
 import ConnectionsController from './controllers/ConnectionsController';
 import UsersController from './controllers/UsersController';
 import SessionsController from './controllers/SessionsController';
 
-// Authentication middleware
-function authMiddleware(req, res, next) {}
+// Defining authentication middleware
+function authMiddleware(req, res, next) {
+  // Checks if there is an authentication header
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token was not provided' });
+  }
+
+  // Gets only the actual token
+  const [, token] = authHeader.split(' ');
+
+  // Verifies token
+  jwt.verify(token, authConfig.secret, function (err, decodedToken) {
+    if (err) {
+      return res.status(401).json({ error: 'Invalid token' });
+    } else {
+      req.userId = decodedToken.id;
+      req.userEmail = decodedToken.email;
+      next();
+    }
+  });
+}
 
 const routes = express.Router();
 const classesController = new ClassesController();
@@ -20,6 +41,9 @@ routes.post('/users', usersController.create);
 
 // Sessions (Authentication)
 routes.post('/login', sessionsController.create);
+
+// Auth Mddleware
+routes.use(authMiddleware);
 
 // Classes
 routes.get('/classes', classesController.index);
