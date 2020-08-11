0591f80cd0ab93d734946064930af1acd7450cf5
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2020-08-11 01:57:10 -0300

    route to create new user and encrypt password

--- server/src/controllers/UsersController.ts
+++ server/src/controllers/UsersController.ts
@@ -1,8 +1,35 @@
 import { Request, Response } from 'express';
 import db from '../database/connection';
+const bcrypt = require('bcryptjs');
 
 export default class UsersController {
   async create(req: Request, res: Response) {
-    return res.status(200).json({ message: 'Test' });
+    const { first_name, last_name, email, password } = req.body;
+
+    const password_hash = await bcrypt.hash(password, 10);
+
+    // To compare later:
+    // bcrypt.compare(password, password_hash);
+
+    const trx = await db.transaction();
+
+    try {
+      await trx('users').insert({
+        first_name,
+        last_name,
+        email,
+        password_hash,
+      });
+
+      await trx.commit();
+    } catch (err) {
+      await trx.rollback();
+      console.log('Error inserting data on database');
+      return res.status(400).json({
+        error: 'Unexpected error while creating new user',
+      });
+    }
+
+    return res.status(200).json({ message: 'User created' });
   }
 }
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import ClassesController from './controllers/ClassesController';
 import ConnectionsController from './controllers/ConnectionsController';
+import UsersController from './controllers/UsersController';
 
 const routes = express.Router();
 const classesController = new ClassesController();
 const connectionsController = new ConnectionsController();
+const usersController = new UsersController();
 
 // Test route
 routes.get('/', (req, res) => {
