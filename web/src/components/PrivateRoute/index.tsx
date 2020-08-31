1d7dffa91e7e192579fad391638961f223632073
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2020-08-31 01:22:05 -0300

    implementend existing token validation

--- web/src/components/PrivateRoute/index.tsx
+++ web/src/components/PrivateRoute/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
-
+import api from '../../services/api';
 import { Route, Redirect } from 'react-router-dom';
 
 const PrivateRoute = (props: any) => {
+  async function checksIfTokenIsValid() {
+    api
+      .get('authcheck', {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('proffy-token')}`,
+        },
+      })
+      .then((res) => {
+        console.log('caiu no then');
+        return true;
+      })
+      .catch((err) => {
+        console.log('caiu no catch');
+        return false;
+      });
+  }
+
   const isAuthenticated = !!localStorage.getItem('proffy-token');
-  return isAuthenticated ? <Route {...props} /> : <Redirect to='/login' />;
+
+  const tokenIsValid = checksIfTokenIsValid();
+
+  console.log(tokenIsValid);
+  return isAuthenticated && tokenIsValid ? (
+    <Route {...props} />
+  ) : (
+    <Redirect to='/login' />
+  );
 };
 
 export default PrivateRoute;
